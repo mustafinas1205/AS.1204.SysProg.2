0a6bc3bd326a2e67460ef9138914bbe05e21516c
dl <dl@bugtraq.ru>
2015-06-17 01:03:04 +0300

    (!) первый тур, первый подход

--- coursework2015/Robots/robotbase.08/robotbase.cpp
+++ coursework2015/Robots/robotbase.08/robotbase.cpp
@@ -3,29 +3,308 @@
 
 #include "stdafx.h"
 #include "robotbase.h"
+#include "cmath"
 
 using namespace std;
 
-extern "C" 
+
+extern "C"
 void DoStep(stepinfo *Info, step *Step)
 {
-	//AfxMessageBox("DLL");
-	srand(time(0));
-	/*Step->action = ACT_MOVE;
-	Step->dx = rand() % 3 -1;
-	Step->dy = rand() % 3 -1;*/
-	/*Step->numberOfActions = 1;
-	Step->actions = new action[Step->numberOfActions];
-	Step->actions[0].act = ACT_MOVE;
-	Step->actions[0].dx = rand() % 3 - 1;
-	Step->actions[0].dy = rand() % 3 - 1;*/
-	//SetEvent(CommitStep);
-	/*int myNumber = Info->yourNumber;
-	int myX = Info->robots[myNumber]->x;
-	int myY = Info->robots[myNumber]->y;*/
+	int p = 0;
+	int q = 0;
+	int a = 0;
+	int j = 0;
+	int h = 0;
+	int NeX1, NeY1, NlX1, NlY1, xk1, yk1, k1;
+	double minDistNe = Info->field->fieldHeight * 2;
+	double minDistNl = Info->field->fieldHeight * 2;
+	double minDistR = Info->field->fieldHeight * 2;
+	int im = Info->yourNumber;
+	int id = Info->robots[im]->playerid;
+	int meX = Info->robots[im]->x;
+	int meY = Info->robots[im]->y;
 
-	DoAction(Step, ACT_MOVE, rand() % 3 -1, rand() % 3 -1);
+	for (int k = 0; k < Info->field->rivals; k++)
+	{
+		if (id == Info->robots[k]->playerid)
+			j = j + 1;//считаем количество собственных роботов на поле
+	}
+
+	for (int l = 0; l < (Info->field->rivals-j+1); l++)
+	{
+		if (Info->robots[l]->alive == false)
+			h = h + 1;//считаем количество умерших роботов
+	}
+
+	if (!(h == Info->field->rivals - j) && (Info->stepnum<ceil(0.93*Info->field->N))) 
+	{
+	
+	for (int k = 0; k < Info->field->rivals; k++)
+	{
+		if (!(k == im) && (Info->robots[k]->E == 0))
+		{
+			int xk = Info->robots[k]->x;
+			int yk = Info->robots[k]->y;
+			double dm = sqrt((xk - meX)*(xk - meX) + (yk - meY)*(yk - meY));
+			double nowmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+			if (nowmove >= dm)
+			{
+				q = 1;
+				DoAction(Step, ACT_MOVE, xk - meX, yk - meY);
+				DoAction(Step, ACT_TECH, 0, (Info->robots[im]->L) * 2 / 3, (Info->robots[im]->L) / 3);
+			}
+		}
+	}
+	if ((Info->robots[im]->E <= ((Info->field->Emax) * 0.5)) && ((Info->field->Ne) > 0) && (q == 0))
+	{
+		for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+		{
+			if (Info->objects[k]->type == OBJ_CHARGER)
+			{
+				int NeX = Info->objects[k]->x;
+				int NeY = Info->objects[k]->y;
+				double DistNe = sqrt((NeX - meX)*(NeX - meX) + (NeY - meY)*(NeY - meY));
+				if (DistNe < minDistNe)
+				{
+					minDistNe = DistNe;
+					NeX1 = NeX;
+					NeY1 = NeY;
+				}
+			}
+		}
+		if (((Info->robots[im]->L) - (Info->field->Vmax)) >= 0)
+		{
+			DoAction(Step, ACT_TECH, 0, ((Info->robots[im]->L) - (Info->field->Vmax)), Info->field->Vmax);
+		}
+
+		else//выполняем поиск ближайшей кормушки с ТО и едим оттуда
+		{
+			if (((Info->robots[im]->L) - (Info->field->Vmax)) < 0 && ((Info->field->Nl) > 0))
+			{
+				p = 1;
+				for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+				{
+					if (Info->objects[k]->type == OBJ_TECH)
+					{
+						int NlX = Info->objects[k]->x;
+						int NlY = Info->objects[k]->y;
+						double DistNl = sqrt((NlX - meX)*(NlX - meX) + (NlY - meY)*(NlY - meY));
+						if (DistNl < minDistNl)
+						{
+							minDistNl = DistNl;
+							NlX1 = NlX;
+							NlY1 = NlY;
+						}
+					}
+				}
+				double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+				if (minDistNl <= maxmove)
+					DoAction(Step, ACT_MOVE, NlX1 - meX, NlY1 - meY);
+				else
+					DoAction(Step, ACT_MOVE, ceil(abs(NlX1 - meX) / (minDistNl / maxmove)), ceil((abs(NlY1 - meY) / (minDistNl / maxmove))));
+			}
+		}
+		double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+		if (minDistNe <= maxmove && p == 0)
+		{
+			DoAction(Step, ACT_MOVE, NeX1 - meX, NeY1 - meY);
+		}
+		else if (p == 0)
+		{
+			DoAction(Step, ACT_MOVE, ceil(abs(NeX1 - meX) / (minDistNe / maxmove)), ceil(abs(NeY1 - meY) / (minDistNe / maxmove)));
+		}
+	}
+	//кормушки с ТО
+	else if ((Info->robots[im]->L < ((Info->field->Lmax) * 0.5)) && ((Info->field->Nl) > 0) && (p == 0) && (q == 0))
+	{
+		for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+		{
+			if (Info->objects[k]->type == OBJ_TECH)
+			{
+				int NlX = Info->objects[k]->x;
+				int NlY = Info->objects[k]->y;
+				double DistNl = sqrt((NlX - meX)*(NlX - meX) + (NlY - meY)*(NlY - meY));
+				if (DistNl < minDistNl)
+				{
+					minDistNl = DistNl;
+					NlX1 = NlX;
+					NlY1 = NlY;
+				}
+			}
+		}
+		if (((Info->robots[im]->L) - (Info->field->Vmax)) >= 0)
+			DoAction(Step, ACT_TECH, 0, ((Info->robots[im]->L) - (Info->field->Vmax)), Info->field->Vmax);
+		else
+			DoAction(Step, ACT_TECH, 0, 0, Info->robots[im]->L);
+
+		double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+
+		if (minDistNl <= maxmove)
+			DoAction(Step, ACT_MOVE, NlX1 - meX, NlY1 - meY);
+		else
+			DoAction(Step, ACT_MOVE, ceil(abs(NlX1 - meX) / (minDistNl / maxmove)), ceil(abs(NlY1 - meY) / (minDistNl / maxmove)));
+	}
+	else if (q == 0)//начинаем работу с атакой
+	{
+		for (int k = 0; k < Info->field->rivals; k++)
+		{
+			if ((!(k == im)) && (Info->robots[k]->alive == true) && (!(Info->robots[k]->playerid==id)))
+			{
+				int xk = Info->robots[k]->x;
+				int yk = Info->robots[k]->y;
+				float dm = sqrt((xk - meX)*(xk - meX) + (yk - meY)*(yk - meY));
+				if (dm < minDistR)
+				{
+					minDistR = dm;
+					k1 = k;
+					xk1 = xk;
+					yk1 = yk;
+				}
+			}
+		}
+		
+		if (Info->robots[im]->L >= Info->field->Vmax)
+		{
+			double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+
+
+			if (minDistR <= maxmove && minDistR > 1)
+			{
+				a = 1;
+				DoAction(Step, ACT_MOVE, xk1 - meX, yk1 - meY - 1);
+				if ((Info->robots[k1]->P) <= Info->robots[im]->L && (!(yk1 == meY)))
+				{
+					if (Info->robots[im]->L - Info->robots[k1]->P <= Info->field->Vmax)
+						DoAction(Step, ACT_TECH, (Info->robots[k1]->P), 0, Info->robots[im]->L - Info->robots[k1]->P);
+					else
+						DoAction(Step, ACT_TECH, Info->robots[im]->L - Info->field->Vmax, 0, Info->field->Vmax);
+					DoAction(Step, ACT_ATTACK, xk1 - meX, yk1 - meY);
+				}
+			}
+			else if (minDistR > maxmove)
+			{
+				DoAction(Step, ACT_MOVE, ceil(abs(xk1 - meX) / (minDistR / maxmove)), ceil(abs(yk1 - meY) / (minDistR / maxmove)));
+				DoAction(Step, ACT_TECH, 0, Info->robots[im]->L - Info->field->Vmax, Info->field->Vmax);
+			}
+			else if (minDistR <= 1)
+			{
+				if ((Info->robots[k1]->P) <= (Info->robots[im]->L) && (!(yk1 == meY)))
+				{
+					if (Info->robots[im]->L - Info->robots[k1]->P <= Info->field->Vmax)
+						DoAction(Step, ACT_TECH, (Info->robots[k1]->P), 0, Info->robots[im]->L - Info->robots[k1]->P);
+					else
+						DoAction(Step, ACT_TECH, Info->robots[im]->L - Info->field->Vmax, 0, Info->field->Vmax);
+					DoAction(Step, ACT_ATTACK, xk1 - meX, yk1 - meY);
+				}
+			}
+		}
+		else//опять идем кушать на станцию ТО
+		{
+			if (((Info->robots[im]->L) - (Info->field->Vmax)) < 0 && ((Info->field->Nl) > 0))
+			{
+				p = 1;
+				for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+				{
+					if (Info->objects[k]->type == OBJ_TECH)
+					{
+						int NlX = Info->objects[k]->x;
+						int NlY = Info->objects[k]->y;
+						double DistNl = sqrt((NlX - meX)*(NlX - meX) + (NlY - meY)*(NlY - meY));
+						if (DistNl < minDistNl)
+						{
+							minDistNl = DistNl;
+							NlX1 = NlX;
+							NlY1 = NlY;
+						}
+					}
+				}
+
+				double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+
+				if (minDistNl < maxmove)
+				{
+					DoAction(Step, ACT_MOVE, NlX1 - meX, NlY1 - meY);
+				}
+				else
+				{
+					DoAction(Step, ACT_MOVE, ceil(abs(NlX1 - meX) / (minDistNl / maxmove)), ceil(abs(NlY1 - meY) / (minDistNl / maxmove)));
+				}
+			}
+		}
+	}
+	}
+
+	else//прячемся в кормушку с энергией
+	{
+	
+	for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+	{
+		if (Info->objects[k]->type == OBJ_CHARGER)
+		{
+			int NeX = Info->objects[k]->x;
+			int NeY = Info->objects[k]->y;
+			double DistNe = sqrt((NeX - meX)*(NeX - meX) + (NeY - meY)*(NeY - meY));
+			if (DistNe < minDistNe)
+			{
+				minDistNe = DistNe;
+				NeX1 = NeX;
+				NeY1 = NeY;
+			}
+		}
+	}
+	if (((Info->robots[im]->L) - (Info->field->Vmax)) >= 0)
+	{
+		DoAction(Step, ACT_TECH, 0, ((Info->robots[im]->L) - (Info->field->Vmax)), Info->field->Vmax);
+	}
+
+	else//выполняем поиск ближайшей кормушки с ТО и едим оттуда
+	{
+		if (((Info->robots[im]->L) - (Info->field->Vmax)) < 0 && ((Info->field->Nl) > 0))
+		{
+			p = 1;
+			for (int k = 0; k < (Info->field->Ne + Info->field->Nl); k++)
+			{
+				if (Info->objects[k]->type == OBJ_TECH)
+				{
+					int NlX = Info->objects[k]->x;
+					int NlY = Info->objects[k]->y;
+					double DistNl = sqrt((NlX - meX)*(NlX - meX) + (NlY - meY)*(NlY - meY));
+					if (DistNl < minDistNl)
+					{
+						minDistNl = DistNl;
+						NlX1 = NlX;
+						NlY1 = NlY;
+					}
+				}
+			}
+			double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+			if (minDistNl <= maxmove)
+				DoAction(Step, ACT_MOVE, NlX1 - meX, NlY1 - meY);
+			else
+				DoAction(Step, ACT_MOVE, ceil(abs(NlX1 - meX) / (minDistNl / maxmove)), ceil((abs(NlY1 - meY) / (minDistNl / maxmove))));
+		}
+	}
+	double maxmove = (Info->field->Vmax * Info->robots[im]->V * Info->robots[im]->E) / (Info->field->Lmax * Info->field->Emax);
+	if (minDistNe <= maxmove && p == 0)
+	{
+		DoAction(Step, ACT_MOVE, NeX1 - meX, NeY1 - meY);
+	}
+	else if (p == 0)
+	{
+		DoAction(Step, ACT_MOVE, ceil(abs(NeX1 - meX) / (minDistNe / maxmove)), ceil(abs(NeY1 - meY) / (minDistNe / maxmove)));
+	}
+	}
 	return;
 }
+
+
+
+
+
+
+
+
+
 
 
--- coursework2015/Robots/robotbase.11/robotbase.cpp
+++ coursework2015/Robots/robotbase.11/robotbase.cpp
@@ -3,29 +3,366 @@
 
 #include "stdafx.h"
 #include "robotbase.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 extern "C" 
 void DoStep(stepinfo *Info, step *Step)
 {
-	//AfxMessageBox("DLL");
 	srand(time(0));
-	/*Step->action = ACT_MOVE;
-	Step->dx = rand() % 3 -1;
-	Step->dy = rand() % 3 -1;*/
-	/*Step->numberOfActions = 1;
-	Step->actions = new action[Step->numberOfActions];
-	Step->actions[0].act = ACT_MOVE;
-	Step->actions[0].dx = rand() % 3 - 1;
-	Step->actions[0].dy = rand() % 3 - 1;*/
-	//SetEvent(CommitStep);
-	/*int myNumber = Info->yourNumber;
-	int myX = Info->robots[myNumber]->x;
-	int myY = Info->robots[myNumber]->y;*/
+	int myNumber = Info->yourNumber;//мой номер
+	int myE = Info->robots[myNumber]->E;//мое значение энергии
+	int myL = Info->robots[myNumber]->L;//тех сост текущее
+	int myX = Info->robots[myNumber]->x;//мои координаты
+	int myY = Info->robots[myNumber]->y;
+	int myId = Info->robots[myNumber]->playerid;
+	int n = Info->field->rivals;// ол-во роботов на поле
+	int *A = new int[n];//массив атак
+	int *P = new int[n];//массив защиты
+	int *V = new int[n];//массив скорости
+	int *E = new int[n];//массив энергии
+	int *x = new int[n];//массив координат
+	int *y = new int[n];//массив
+	double radattack = (Info->field->Rmax)*(Info->robots[myNumber]->V) / (Info->field->Lmax)*(Info->robots[myNumber]->E) / (Info->field->Emax);//радиус удара
+	int move = (int)((Info->field->Vmax)*(Info->robots[myNumber]->V) / (Info->field->Lmax)*(Info->robots[myNumber]->E) / (Info->field->Emax));//дальность шага
+	int Ne = Info->field->Ne;//узнаем скоько их
+	int Nl = Info->field->Nl;
 
-	DoAction(Step, ACT_MOVE, rand() % 3 -1, rand() % 3 -1);
-	return;
+
+	///////////////////////////////////////////////////////////
+	////1.¬еселье с файлами
+	///////////////////////////////////////////////////////////
+	if ((Info->stepnum) != 1)//если шаг равен перевому, считывать нечего
+	{
+		for (int i = 0; i < n; i++)//считываем c файла
+		{
+			char ch[69];
+			sprintf(ch, "../Robots/robotbase.11/Robot%d", i);
+			string fileName = ch;
+			fileName += ".txt";
+			ifstream file(fileName);
+			file >> A[i] >> P[i] >> V[i] >> E[i] >> x[i] >> y[i];//все записываем в массив
+			file.close();
+		}
+	}
+
+
+	for (int i = 0; i < n; i++)//записываем в файл
+	{
+
+		char ch[69];
+		sprintf(ch, "../Robots/robotbase.11/Robot%d", i);
+		string fileName = ch;
+		fileName += ".txt";
+		ofstream file(fileName);
+		file << Info->robots[i]->A << endl << Info->robots[i]->P << endl << Info->robots[i]->V << endl << Info->robots[i]->E << endl << Info->robots[i]->x << endl << Info->robots[i]->y;
+		file.close();
+	}
+
+
+	/////////////////////////////////////////////////////////////
+	////2.Ќаходим ближайшего робота
+	/////////////////////////////////////////////////////////////
+	double realx = Info->field->fieldWidth;
+	int realy = 0;
+
+	ifstream file("../Robots/robotbase.11/test.txt");
+	file >> realy;//запоминаем индекс
+	file.close();
+
+	if (Info->robots[realy]->E < 0 || Info->stepnum == 1)
+	{
+		for (int i = 0; i<Info->field->rivals; i++)
+		{
+			int id = Info->robots[i]->playerid;
+			if (id!=myId)
+			{
+				double x = sqrt(pow(Info->robots[i]->x - myX, 2) + pow(Info->robots[i]->y - myY, 2));
+				if (x < realx)
+				{
+					realx = x;
+					realy = i;
+					ofstream file("../Robots/robotbase.11/test.txt");
+					file << realy << endl;//все записываем в массив
+					file.close();
+				}
+			}
+
+		}
+	}
+
+
+
+	/////////////////////////////////////////////////////////////
+	////3.Ѕлижайша€ кормушка(энерги€)
+	////////////////////////////////////////////////////////////
+
+	double realxe = Info->field->fieldWidth;
+	int realye = 0;
+	for (int i = 0; i < Ne + Nl; i++)
+	{
+		if (Info->objects[i]->type == OBJ_CHARGER)
+		{
+			double xe = pow(pow((Info->objects[i]->x - myX), 2) + pow((Info->objects[i]->y - myY), 2), 1.0 / 2);
+			if (realxe > xe)
+			{
+				realxe = xe;
+				realye = i;
+			}
+		}
+	}
+	/////////////////////////////////////////////////////////////
+	////4.Ѕлижайша€ кормушка(тех)
+	////////////////////////////////////////////////////////////
+	double realxl = Info->field->fieldWidth;
+	int realyl = 0;
+	for (int i = 0; i < Ne + Nl; i++)
+	{
+		if (Info->objects[i]->type == OBJ_TECH)
+		{
+			double xl = pow(pow((Info->objects[i]->x - myX), 2) + pow((Info->objects[i]->y - myY), 2), 1.0 / 2);
+			if (realxl > xl)
+			{
+				realxl = xl;
+				realyl = i;
+			}
+		}
+	}
+
+	/////////////////////////////////////////////////////////////
+	////5. ол-во живых роботов
+	////////////////////////////////////////////////////////////
+	int nalive = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (Info->robots[i]->alive == true)
+		{
+			nalive++;
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////
+	if ((realxe > 0) && (realxl > 0))
+	{
+		if (myE <= 0.6*Info->field->Emax && myL <= 0.6*Info->field->Lmax)//≈сли энерги€ и тех меньше критического, мы бежим в кормушку
+		{//сначала бежим за энергией
+			//¬ычисл€ем координаты относительно моих
+			int dx = (Info->objects[realye]->x) - myX;
+			int dy = (Info->objects[realye]->y) - myY;
+			double  h = realxe / move;
+			if (h < 1)
+			{
+				int dx = (Info->objects[realye]->x) - myX;
+				int dy = (Info->objects[realye]->y) - myY;
+				DoAction(Step, ACT_MOVE, dx, dy);
+
+			}
+			else//вычисл€ем промежуточные шаги
+			{
+				int newx = ((Info->objects[realye]->x) - myX) / h;
+				int newy = ((Info->objects[realye]->y) - myY) / h;
+				DoAction(Step, ACT_MOVE, newx, newy);
+			}
+		}
+
+		///////////////////////////////////////////////////////////////////
+
+		if (myE >= 0.6*Info->field->Emax && myL <= 0.6*Info->field->Lmax)//≈сли тех меньше критического, мы бежим в кормушку
+		{
+			int dx = (Info->objects[realyl]->x) - myX;
+			int dy = (Info->objects[realyl]->y) - myY;
+			double  h = realxl / move;
+			if (h < 1)
+			{
+				int dx = (Info->objects[realyl]->x) - myX;
+				int dy = (Info->objects[realyl]->y) - myY;
+				DoAction(Step, ACT_MOVE, dx, dy);
+			}
+			else//вычисл€ем промежуточные шаги
+			{
+				int newx = ((Info->objects[realyl]->x) - myX) / h;
+				int newy = ((Info->objects[realyl]->y) - myY) / h;
+				DoAction(Step, ACT_MOVE, newx, newy);
+			}
+		}
+
+		///////////////////////////////////////////////////////////////
+
+		if (myE <= 0.6*Info->field->Emax && myL >= 0.6*Info->field->Lmax)//≈сли энерги€ меньше критического, мы бежим в кормушку
+		{//¬ычисл€ем координаты относительно моих
+			double  h = realxe / move;
+			if (h < 1)
+			{
+				int dx = (Info->objects[realye]->x) - myX;
+				int dy = (Info->objects[realye]->y) - myY;
+				DoAction(Step, ACT_MOVE, dx, dy);
+
+			}
+			else//вычисл€ем промежуточные шаги
+			{
+				int newx = ((Info->objects[realye]->x) - myX) / h;
+				int newy = ((Info->objects[realye]->y) - myY) / h;
+				DoAction(Step, ACT_MOVE, newx, newy);
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+
+		if (myE > 0.6*Info->field->Emax && myL > 0.6*Info->field->Lmax)
+		{
+			if (nalive >= 3)//////////////////////ATTACK
+			{
+				int newA = (int)(0.35*myL);
+				int newP = (int)(0.15* myL);
+				int newV = (int)(0.5*myL);
+				DoAction(Step, ACT_TECH, newA, newP, newV);
+				int myP = Info->robots[myNumber]->P;
+				int myA = Info->robots[myNumber]->A;
+				int enP = Info->robots[myNumber]->P;
+				int enA = Info->robots[myNumber]->A;
+				if (enP < (0.7*Info->field->Lmax) && enA <= myA + 0.05*(Info->field->Lmax))
+				{
+					double  h = realx / move;
+					if (h < 1)
+					{
+						int dx = (Info->robots[realy]->x) - myX;
+						int dy = (Info->robots[realy]->y) - myY;
+						DoAction(Step, ACT_MOVE, dx - 1, dy);
+						if ((Info->robots[realy]->x) != myX || (Info->robots[realy]->y) != myY)
+						{
+							DoAction(Step, ACT_ATTACK, dx, dy);
+						}
+
+					}
+					else//вычисл€ем промежуточные шаги
+					{
+						int newx = ((Info->robots[realy]->x) - myX) / h;
+						int newy = ((Info->robots[realy]->y) - myY) / h;
+						DoAction(Step, ACT_MOVE, newx, newy);
+					}
+				}
+				if (enP > (0.7*Info->field->Lmax))
+				{
+					if (myE < 700)
+					{
+						int dx = (Info->objects[realye]->x) - myX;
+						int dy = (Info->objects[realye]->y) - myY;
+						double  h = realxe / move;
+						if (h < 1)
+						{
+							int dx = (Info->objects[realye]->x) - myX;
+							int dy = (Info->objects[realye]->y) - myY;
+							DoAction(Step, ACT_MOVE, dx, dy);
+
+						}
+						else//вычисл€ем промежуточные шаги
+						{
+							int newx = ((Info->objects[realye]->x) - myX) / h;
+							int newy = ((Info->objects[realye]->y) - myY) / h;
+							DoAction(Step, ACT_MOVE, newx, newy);
+						}
+					}
+					else
+					{
+						int newA = (int)(0.6*myL);
+						int newP = (int)(0.1* myL);
+						int newV = (int)(0.3*myL);
+						DoAction(Step, ACT_TECH, newA, newP, newV);
+						double  h = realx / move;
+						if (h < 1)
+						{
+							int dx = (Info->robots[realy]->x) - myX;
+							int dy = (Info->robots[realy]->y) - myY;
+							DoAction(Step, ACT_MOVE, dx - 1, dy);
+							if ((Info->robots[realy]->x) != myX || (Info->robots[realy]->y) != myY)
+							{
+								DoAction(Step, ACT_ATTACK, dx, dy);
+							}
+
+						}
+						else//вычисл€ем промежуточные шаги
+						{
+							int newx = ((Info->robots[realy]->x) - myX) / h;
+							int newy = ((Info->robots[realy]->y) - myY) / h;
+							DoAction(Step, ACT_MOVE, newx, newy);
+						}
+					}
+
+				}
+				if (enP < (0.7*Info->field->Lmax) && enA > myA + 0.05*(Info->field->Lmax))
+				{
+					int newA = (int)(0.3*myL);
+					int newP = (int)(0.2* myL);
+					int newV = (int)(0.5*myL);
+					DoAction(Step, ACT_TECH, newA, newP, newV);
+					double  h = realx / move;
+					if (h < 1)
+					{
+						int dx = (Info->robots[realy]->x) - myX;
+						int dy = (Info->robots[realy]->y) - myY;
+						DoAction(Step, ACT_MOVE, dx - 1, dy);
+						if ((Info->robots[realy]->x) != myX || (Info->robots[realy]->y) != myY)
+						{
+							DoAction(Step, ACT_ATTACK, dx, dy);
+						}
+
+					}
+					else//вычисл€ем промежуточные шаги
+					{
+						int newx = ((Info->robots[realy]->x) - myX) / h;
+						int newy = ((Info->robots[realy]->y) - myY) / h;
+						DoAction(Step, ACT_MOVE, newx, newy);
+					}
+				}
+			}
+			if (nalive < 3 || Info->stepnum == (Info->field->N - 0.2*Info->field->N))
+			{
+				double  h = realxe / move;
+				if (h < 1)
+				{
+					int dx = (Info->objects[realye]->x) - myX;
+					int dy = (Info->objects[realye]->y) - myY;
+					DoAction(Step, ACT_MOVE, dx, dy);
+					int newA = (int)(0 * myL);
+					int newP = (int)(myL);
+					int newV = (int)(0 * myL);
+					DoAction(Step, ACT_TECH, newA, newP, newV);
+
+				}
+				else//вычисл€ем промежуточные шаги
+				{
+					int newx = ((Info->objects[realye]->x) - myX) / h;
+					int newy = ((Info->objects[realye]->y) - myY) / h;
+					DoAction(Step, ACT_MOVE, newx, newy);
+					int newA = (int)(0 * myL);
+					int newP = (int)(myL);
+					int newV = (int)(0 * myL);
+					DoAction(Step, ACT_TECH, newA, newP, newV);
+				}
+
+			}
+		}
+	}
+	/////////////////////////////////////////////////////////////////////////
+	if (realxe == 0 && ((Info->robots[myNumber]->E) != (Info->field->Emax)))
+	{
+		return;
+	}
+	if (realxe == 0 && ((Info->robots[myNumber]->E) == (Info->field->Emax)))
+	{
+		DoAction(Step, ACT_MOVE, -1, -1);
+	}
+	if (realxl == 0 && ((Info->robots[myNumber]->L) != (Info->field->Lmax)))
+	{
+		return;
+	}
+	if (realxl == 0 && ((Info->robots[myNumber]->L) == (Info->field->Lmax)))
+	{
+		DoAction(Step, ACT_MOVE, -1, -1);
+	}
 }
+
 
 

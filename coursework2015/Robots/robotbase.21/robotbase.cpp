e3ac3eec2b99a895e0716e0ab1a5b61256bebe6b
Walrusteak <yagello.as1204@asugubkin.ru>
2015-06-18 16:28:08 +0300

    Роботы

--- coursework2015/Robots/robotbase.21/robotbase.cpp
+++ coursework2015/Robots/robotbase.21/robotbase.cpp
@@ -3,28 +3,430 @@
 
 #include "stdafx.h"
 #include "robotbase.h"
+#include <fstream>
 
 using namespace std;
 
 extern "C" 
+
+int n = 0;
+int id;
+int x;
+int y;
+int E;
+int L;
+
 void DoStep(stepinfo *Info, step *Step)
 {
-	//AfxMessageBox("DLL");
-	srand(time(0));
-	/*Step->action = ACT_MOVE;
-	Step->dx = rand() % 3 -1;
-	Step->dy = rand() % 3 -1;*/
-	/*Step->numberOfActions = 1;
-	Step->actions = new action[Step->numberOfActions];
-	Step->actions[0].act = ACT_MOVE;
-	Step->actions[0].dx = rand() % 3 - 1;
-	Step->actions[0].dy = rand() % 3 - 1;*/
-	//SetEvent(CommitStep);
-	/*int myNumber = Info->yourNumber;
-	int myX = Info->robots[myNumber]->x;
-	int myY = Info->robots[myNumber]->y;*/
+	n = Info->yourNumber;
+	id = Info->robots[n]->playerid;
+	x = Info->robots[n]->x;
+	y = Info->robots[n]->y;
+	E = Info->robots[n]->E;
+	L = Info->robots[n]->L;
 
-	DoAction(Step, ACT_MOVE, rand() % 3 -1, rand() % 3 -1);
+	if (Info->stepnum == 1)
+	{
+		ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+		fStep << 1;
+		fStep.close();
+	}
+
+	if (L < 0.8 * Info->field->Lmax)
+	{
+		ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+		fStep << 4;
+		fStep.close();
+	}
+
+	if (E < 0.4 * Info->field->Emax)
+	{
+		ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+		fStep << 2;
+		fStep.close();
+	}
+
+	int step;
+	ifstream fStep("../Robots/robotbase.21/Yuri.txt");
+	fStep >> step;
+	fStep.close();
+
+	switch(step)
+	{
+	case 1:
+		{
+			DoAction(Step, ACT_TECH, 0.4*L, 0.4*L, 0.2*L);
+			double dist = Info->field->fieldWidth;
+			int v;
+			for (int i = 0; i < Info->field->rivals; i++)
+			{
+				if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+				{
+					int vx = Info->robots[i]->x;
+					int vy = Info->robots[i]->y;
+					double tmpdist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+					if (tmpdist < dist)
+					{
+						dist = tmpdist;
+						v = i;
+					}
+				}
+			}
+			int vx = Info->robots[v]->x;
+			int vy = Info->robots[v]->y;
+			double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+			double step = Info->robots[n]->V*Info->field->Vmax/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+			if (dist > attackdist)
+			{
+				int dx, dy;
+				if (dist > step)
+				{
+					dx = (vx - x)*step/dist;
+					dy = (vy - y)*step/dist;
+				}
+				else
+				{
+					dx = vx - x;
+					dy = vy - y;
+					if (vx > x)
+						dx-=1;
+					else if (vx < x)
+						dx+=1;
+					else if (vy > y)
+						dy-=1;
+					else dy+=1;
+				}
+				DoAction(Step, ACT_MOVE, dx, dy);
+			}
+			else
+			{
+				double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+				double P = Info->robots[v]->P * Info->robots[v]->E / Info->field->Emax;
+				int dx, dy;
+				dx = Info->robots[v]->x - Info->robots[n]->x;
+				dy = Info->robots[v]->y - Info->robots[n]->y;
+				if (A>=P)
+					DoAction(Step, ACT_ATTACK, dx, dy);
+				ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+				fStep << 2;
+				fStep.close();
+			}
+			break;
+		}
+	case 2:
+		{
+			DoAction(Step, ACT_TECH, 0.4*L, 0.1*L, 0.5*L);
+			int eid = 0;
+			int ex = Info->objects[eid]->x;
+			int ey = Info->objects[eid]->y;
+			double mindist = sqrt(pow(ex-x, 2) + pow(ey-y, 2));
+			for (int i = 1; i<Info->field->Ne; i++)
+			{
+				ex = Info->objects[i]->x;
+				ey = Info->objects[i]->y;
+				double curdist = sqrt(pow(ex-x, 2) + pow(ey-y, 2));
+				if (curdist < mindist)
+				{
+					mindist = curdist;
+					eid = i;
+				}
+			}
+			double maxstep = Info->robots[n]->V*Info->field->Vmax/Info->field->Lmax*E/Info->field->Emax;	
+			ex = Info->objects[eid]->x;
+			ey = Info->objects[eid]->y;
+			if (maxstep < mindist)
+			{
+				int dx, dy;
+				dx = (ex - x)*maxstep/mindist;
+				dy = (ey - y)*maxstep/mindist;
+				DoAction(Step, ACT_MOVE, dx, dy);
+			}
+			else
+			{
+				int dx, dy;
+				dx = ex - x;
+				dy = ey - y;
+				DoAction(Step, ACT_MOVE, dx, dy);
+				DoAction(Step, ACT_TECH, 0.4*L, 0.3*L, 0.3*L);
+				ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+				fStep << 3;
+				fStep.close();
+			}
+
+			bool found = false;
+			int v;
+			double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+			double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+			for (int i = 0; i < Info->field->rivals; i++)
+			{
+				if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+				{
+					int vx = Info->robots[i]->x;
+					int vy = Info->robots[i]->y;
+					double robodist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+					if (robodist < attackdist)
+					{
+						double vP = Info->robots[i]->P * Info->robots[i]->E / Info->field->Emax;
+						if (vP < A)
+						{
+							v = i;
+							found = true;
+							break;
+						}
+					}
+				}
+			}
+			if (found)
+			{
+				int vx = Info->robots[v]->x;
+				int vy = Info->robots[v]->y;
+				int dx = vx - x;
+				int dy = vy - y;
+				DoAction(Step, ACT_ATTACK, dx, dy);
+			}
+			break;
+		}
+	case 3:
+		{
+			DoAction(Step, ACT_TECH, 0.4*L, 0.3*L, 0.3*L);
+			if (E < Info->field->Emax * 0.99)
+			{
+				bool found = false;
+				int v;
+				double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+				double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+				for (int i = 0; i < Info->field->rivals; i++)
+				{
+					if (Info->robots[i]->playerid != id)
+					{
+						int vx = Info->robots[i]->x;
+						int vy = Info->robots[i]->y;
+						double robodist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+						if (robodist < attackdist)
+						{
+							double vP = Info->robots[i]->P * Info->robots[i]->E / Info->field->Emax;
+							if (vP < A)
+							{
+								v = i;
+								found = true;
+								break;
+							}
+						}
+					}
+				}
+				if (found)
+				{
+					int vx = Info->robots[v]->x;
+					int vy = Info->robots[v]->y;
+					int dx = vx - x;
+					int dy = vy - y;
+					DoAction(Step, ACT_ATTACK, dx, dy);
+				}
+			}
+			else
+			{
+				DoAction(Step, ACT_TECH, 0.4*L, 0.1*L, 0.5*L);
+				ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+				fStep << 4;
+				fStep.close();
+			}
+			break;
+		}
+	case 4:
+		{
+			DoAction(Step, ACT_TECH, 0.4*L, 0.1*L, 0.5*L);
+			int lid = Info->field->Ne;
+			int lx = Info->objects[lid]->x;
+			int ly = Info->objects[lid]->y;
+			double mindist = sqrt(pow(lx-x, 2) + pow(ly-y, 2));
+			for (int i = Info->field->Ne+1; i<Info->field->Ne+Info->field->Nl; i++)
+			{
+				lx = Info->objects[i]->x;
+				ly = Info->objects[i]->y;
+				double curdist = sqrt(pow(lx-x, 2) + pow(ly-y, 2));
+				if (curdist < mindist)
+				{
+					mindist = curdist;
+					lid = i;
+				}
+			}
+			double maxstep = Info->robots[n]->V*Info->field->Vmax/Info->field->Lmax*E/Info->field->Emax;	
+			lx = Info->objects[lid]->x;
+			ly = Info->objects[lid]->y;
+			if (maxstep < mindist)
+			{
+				int dx, dy;
+				dx = (lx - x)*maxstep/mindist;
+				dy = (ly - y)*maxstep/mindist;
+				DoAction(Step, ACT_MOVE, dx, dy);
+			}
+			else
+			{
+				int dx, dy;
+				dx = lx - x;
+				dy = ly - y;
+				DoAction(Step, ACT_MOVE, dx, dy);
+				DoAction(Step, ACT_TECH, 0.4*L, 0.3*L, 0.3*L);
+				ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+				fStep << 5;
+				fStep.close();
+			}
+
+			bool found = false;
+			int v;
+			double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+			double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+			for (int i = 0; i < Info->field->rivals; i++)
+			{
+				if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+				{
+					int vx = Info->robots[i]->x;
+					int vy = Info->robots[i]->y;
+					double robodist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+					if (robodist < attackdist)
+					{
+						double vP = Info->robots[i]->P * Info->robots[i]->E / Info->field->Emax;
+						if (vP < A)
+						{
+							v = i;
+							found = true;
+							break;
+						}
+					}
+				}
+			}
+			if (found)
+			{
+				int vx = Info->robots[v]->x;
+				int vy = Info->robots[v]->y;
+				int dx = vx - x;
+				int dy = vy - y;
+				DoAction(Step, ACT_ATTACK, dx, dy);
+			}
+			break;
+		}
+	case 5:
+		{
+			DoAction(Step, ACT_TECH, 0.4*L, 0.3*L, 0.3*L);
+			if (L < Info->field->Lmax)
+			{
+				bool found = false;
+				int v;
+				double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+				double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+				for (int i = 0; i < Info->field->rivals; i++)
+				{
+					if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+					{
+						int vx = Info->robots[i]->x;
+						int vy = Info->robots[i]->y;
+						double robodist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+						if (robodist < attackdist)
+						{
+							double vP = Info->robots[i]->P * Info->robots[i]->E / Info->field->Emax;
+							if (vP < A)
+							{
+								v = i;
+								found = true;
+								break;
+							}
+						}
+					}
+				}
+				if (found)
+				{
+					int vx = Info->robots[v]->x;
+					int vy = Info->robots[v]->y;
+					int dx = vx - x;
+					int dy = vy - y;
+					DoAction(Step, ACT_ATTACK, dx, dy);
+				}
+			}
+			else
+			{
+				if (E > 0.4 * Info->field->Emax)
+				{
+					bool found = false;
+					double dist = Info->field->fieldWidth;
+					int v;
+					for (int i = 0; i < Info->field->rivals; i++)
+					{
+						if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+						{
+							double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+							double P = Info->robots[i]->P * Info->robots[i]->E / Info->field->Emax;
+							if (A>=P)
+							{
+								int vx = Info->robots[i]->x;
+								int vy = Info->robots[i]->y;
+								double tmpdist = sqrt(pow(vx-x,2) + pow(vy-y,2));
+								if (tmpdist < dist)
+								{
+									dist = tmpdist;
+									v = i;
+									found = true;
+								}
+							}
+						}
+					}
+					if (found)
+					{
+						int vx = Info->robots[v]->x;
+						int vy = Info->robots[v]->y;
+						double attackdist = Info->field->Rmax*Info->robots[n]->V/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+						double step = Info->robots[n]->V*Info->field->Vmax/Info->field->Lmax*Info->robots[n]->E/Info->field->Emax;
+						if (dist > attackdist)
+						{
+							int dx, dy;
+							if (dist > step)
+							{
+								dx = (vx - x)*step/dist;
+								dy = (vy - y)*step/dist;
+							}
+							else
+							{
+								dx = vx - x;
+								dy = vy - y;
+								if (vx > x)
+									dx-=1;
+								else if (vx < x)
+									dx+=1;
+								else if (vy > y)
+									dy-=1;
+								else dy+=1;
+							}
+							DoAction(Step, ACT_MOVE, dx, dy);
+						}
+						else
+						{
+							double A = Info->robots[n]->A * Info->robots[n]->E / Info->field->Emax;
+							double P = Info->robots[v]->P * Info->robots[v]->E / Info->field->Emax;
+							int dx, dy;
+							dx = Info->robots[v]->x - Info->robots[n]->x;
+							dy = Info->robots[v]->y - Info->robots[n]->y;
+							if (A>=P)
+								DoAction(Step, ACT_ATTACK, dx, dy);
+						}
+					}
+					else
+					{
+						DoAction(Step, ACT_TECH, 0.6*L, 0, 0.4*L);
+						ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+						fStep << 2;
+						fStep.close();
+					}
+				}
+				else
+				{
+					DoAction(Step, ACT_TECH, 0.6*L, 0, 0.4*L);
+					ofstream fStep("../Robots/robotbase.21/Yuri.txt");
+					fStep << 2;
+					fStep.close();
+				}
+			}
+			break;
+		}
+	}
 	return;
 }
 
--- coursework2015/Robots/robotbase.23/robotbase.cpp
+++ coursework2015/Robots/robotbase.23/robotbase.cpp
@@ -3,28 +3,229 @@
 
 #include "stdafx.h"
 #include "robotbase.h"
+#include <fstream>
 
 using namespace std;
 
 extern "C" 
 void DoStep(stepinfo *Info, step *Step)
 {
-	//AfxMessageBox("DLL");
-	srand(time(0));
-	/*Step->action = ACT_MOVE;
-	Step->dx = rand() % 3 -1;
-	Step->dy = rand() % 3 -1;*/
-	/*Step->numberOfActions = 1;
-	Step->actions = new action[Step->numberOfActions];
-	Step->actions[0].act = ACT_MOVE;
-	Step->actions[0].dx = rand() % 3 - 1;
-	Step->actions[0].dy = rand() % 3 - 1;*/
-	//SetEvent(CommitStep);
-	/*int myNumber = Info->yourNumber;
-	int myX = Info->robots[myNumber]->x;
-	int myY = Info->robots[myNumber]->y;*/
+	int me = Info->yourNumber;
+	int id = Info->robots[me]->playerid;
+	int cx = Info->robots[me]->x;
+	int cy = Info->robots[me]->y;
+	int cL = Info->robots[me]->L;
+	int cE = Info->robots[me]->E;
+	int charge;
 
-	DoAction(Step, ACT_MOVE, rand() % 3 -1, rand() % 3 -1);
+	if (cE == Info->field->Emax)
+	{
+		ofstream File("../Robots/robotbase.23/charge.txt");
+		File << 0;
+		File.close();
+	}
+
+	ifstream File("../Robots/robotbase.23/charge.txt");
+	File >> charge;
+	File.close();
+
+	if (cL < Info->field->Lmax)
+	{
+		for (int i = Info->field->Ne; i < Info->field->Ne+Info->field->Nl; i++)
+		{
+			if (Info->objects[i]->x == cx && Info->objects[i]->y == cy)
+			{
+				DoAction(Step, ACT_TECH, 0, cL, 0);
+				return;
+			}
+		}
+		int V = Info->field->Vmax;
+		if (V > cL)
+			V = cL;
+		DoAction(Step, ACT_TECH, 0, cL-V, V);
+		int lid = Info->field->Ne;
+		int lx = Info->objects[lid]->x;
+		int ly = Info->objects[lid]->y;
+		double mindist = sqrt(pow(lx-cx, 2) + pow(ly-cy, 2));
+		for (int i = Info->field->Ne+1; i<Info->field->Ne+Info->field->Nl; i++)
+		{
+			lx = Info->objects[i]->x;
+			ly = Info->objects[i]->y;
+			double curdist = sqrt(pow(lx-cx, 2) + pow(ly-cy, 2));
+			if (curdist < mindist)
+			{
+				mindist = curdist;
+				lid = i;
+			}
+		}
+		double maxstep = Info->robots[me]->V*Info->field->Vmax/Info->field->Lmax*cE/Info->field->Emax;	
+		lx = Info->objects[lid]->x;
+		ly = Info->objects[lid]->y;
+		int dx, dy;
+		if (maxstep < mindist)
+		{
+			dx = (lx - cx)*maxstep/mindist;
+			dy = (ly - cy)*maxstep/mindist;
+		}
+		else
+		{
+			dx = lx - cx;
+			dy = ly - cy;
+			DoAction(Step, ACT_TECH, 0, cL, 0);
+			ofstream File("../Robots/robotbase.23/charge.txt");
+			File << 1;
+			File.close();
+		}
+		DoAction(Step, ACT_MOVE, dx, dy);
+		return;
+	}
+	else if (cE < 0.75 * Info->field->Emax || charge)
+	{
+		for (int i = 0; i < Info->field->Ne; i++)
+		{
+			if (Info->objects[i]->x == cx && Info->objects[i]->y == cy)
+			{
+				DoAction(Step, ACT_TECH, 0, cL, 0);
+				return;
+			}
+			int V = Info->field->Vmax;
+			if (V > cL)
+				V = cL;
+			DoAction(Step, ACT_TECH, 0, cL-V, V);
+			int eid = 0;
+			int ex = Info->objects[eid]->x;
+			int ey = Info->objects[eid]->y;
+			double mindist = sqrt(pow(ex-cx, 2) + pow(ey-cy, 2));
+			for (int i = 1; i<Info->field->Ne; i++)
+			{
+				ex = Info->objects[i]->x;
+				ey = Info->objects[i]->y;
+				double curdist = sqrt(pow(ex-cx, 2) + pow(ey-cy, 2));
+				if (curdist < mindist)
+				{
+					mindist = curdist;
+					eid = i;
+				}
+			}
+			double maxstep = Info->robots[me]->V*Info->field->Vmax/Info->field->Lmax*Info->robots[me]->E/Info->field->Emax;	
+			ex = Info->objects[eid]->x;
+			ey = Info->objects[eid]->y;
+			int dx, dy;
+			if (maxstep < mindist)
+			{
+				dx = (ex - cx)*maxstep/mindist;
+				dy = (ey - cy)*maxstep/mindist;
+			}
+			else
+			{
+				dx = ex - cx;
+				dy = ey - cy;
+				DoAction(Step, ACT_TECH, 0, cL, 0);
+				ofstream File("../Robots/robotbase.23/charge.txt");
+				File << 0;
+				File.close();
+			}
+			DoAction(Step, ACT_MOVE, dx, dy);
+		}
+		return;
+	}
+
+
+	double mindist = 123456;
+	int aim = -1;
+	for (int i = 0; i < Info->field->rivals; i++)	//ищем ближайшего
+	{
+		if (Info->robots[i]->alive && Info->robots[i]->playerid != id)
+		{
+			int hx = Info->robots[i]->x;
+			int hy = Info->robots[i]->y;
+			double dist = sqrt(pow(hx-cx,2) + pow(hy-cy,2));
+			if (dist < mindist)
+			{
+				mindist = dist;
+				aim = i;
+			}
+		}
+	}
+
+	if (aim == -1)
+		return;
+
+	int ax = Info->robots[aim]->x;
+	int ay = Info->robots[aim]->y;
+	if (ax > cx)
+		ax-=1;
+	else if (ax < cx)
+		ax+=1;
+	else if (ay > cy)
+		ay-=1;
+	else
+		ay+=1;
+
+	if (Info->robots[aim]->P == Info->robots[aim]->L)	//если враг в полной защите
+	{
+		if (cx != ax || cy != ay)	//если еще не дошли
+		{
+			double maxstep = Info->robots[me]->V*Info->field->Vmax/Info->field->Lmax*cE/Info->field->Emax;
+			int dx, dy;
+			if (maxstep < mindist)
+			{
+				dx = (ax - cx)*maxstep/mindist;
+				dy = (ay - cy)*maxstep/mindist;
+			}
+			else
+			{
+				dx = ax - cx;
+				dy = ay - cy;
+				DoAction(Step, ACT_TECH, 0.89*cL, 0.01*cL, 0.1*cL);
+			}
+			DoAction(Step, ACT_MOVE, dx, dy);
+		}
+		else	//у врага
+		{
+			if (Info->robots[me]->A > 0)
+			{
+				int dx = Info->robots[aim]->x - cx;
+				int dy = Info->robots[aim]->y - cy;
+				DoAction(Step, ACT_ATTACK, dx, dy);
+			}
+		}
+	}
+	else	//если враг не в дефе
+	{
+		double maxattack = Info->field->Rmax*0.2*cL/Info->field->Lmax*cE/Info->field->Emax;
+		if (mindist > maxattack)
+		{
+			int V = Info->field->Vmax;
+			if (V > cL)
+				V = cL;
+			DoAction(Step, ACT_TECH, 0, cL-V, V);
+			double maxstep = Info->robots[me]->V*Info->field->Vmax/Info->field->Lmax*cE/Info->field->Emax;
+			int dx, dy;
+			if (maxstep < mindist)
+			{
+				dx = (ax - cx)*maxstep/mindist;
+				dy = (ay - cy)*maxstep/mindist;
+			}
+			else
+			{
+				dx = ax - cx;
+				dy = ay - cy;
+				DoAction(Step, ACT_TECH, 0.79*cL, 0.01*cL, 0.2*cL);
+			}
+			DoAction(Step, ACT_MOVE, dx, dy);
+		}
+		else
+		{
+			DoAction(Step, ACT_TECH, 0.79*cL, 0.01*cL, 0.2*cL);
+			if (Info->robots[me]->A > 0)
+			{
+				int dx = Info->robots[aim]->x - cx;
+				int dy = Info->robots[aim]->y - cy;
+				DoAction(Step, ACT_ATTACK, dx, dy);
+			}
+		}
+	}
 	return;
 }
 
